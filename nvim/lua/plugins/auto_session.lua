45ee41981d951537ceb201aa029f569a45e2596a
fe2-Nyxar <nyxar49@gmail.com>
2026-09-13 15:20:12 +0100

    update dashboard and startup behavior [skip ci]

--- nvim/lua/plugins/auto_session.lua
+++ nvim/lua/plugins/auto_session.lua
@@ -3,7 +3,11 @@ require("lz.n").load({
 		"auto-session",
 		lazy = false,
 		after = function()
+			local launched_with_args = vim.fn.argc() > 0
+
 			require("auto-session").setup({
+				auto_restore = launched_with_args,
+				auto_save = launched_with_args,
 				suppressed_dirs = { "~/", "/", "/tmp" },
 				session_lens = {
 					picker = "snacks",
