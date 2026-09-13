35424ddf81e8ae946e38b7268dd44d39eaf84cad
fe2-Nyxar <nyxar49@gmail.com>
2026-09-13 02:41:25 +0100

    enable native neovim caching

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,13 @@
+vim.loader.enable()
+
+-- This config does not use remote plugins, and Oil replaces netrw.
+vim.g.loaded_node_provider = 0
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_python3_provider = 0
+vim.g.loaded_ruby_provider = 0
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
 vim.g.nyxar_config_start_time = vim.uv.hrtime()
 
 require("config.options")
